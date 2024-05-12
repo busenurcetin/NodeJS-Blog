9086de505fd66beef00c9154a32528f9d42b82ae
Buse Nur Çetin <busenurcet@gmail.com>
2024-05-12 19:48:39 +0300

    changes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cookieParser = require("cookie-parser");
 const MongoStore = require("connect-mongo");
 const session = require("express-session");
 const methodOverride = require("method-override");
-const {isActiveRoute}= require("./server/helpers/routerHelpers");
+const { isActiveRoute } = require("./server/helpers/routerHelpers");
 
 const connectDB = require("./server/config/db");
 
@@ -16,21 +16,20 @@ const PORT = 5000 || process.env.PORT;
 connectDB();
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
 app.use(cookieParser());
 app.use(methodOverride("_method"));
 
-app.use(session({
-  secret: "keyboard cat",
-  resave: false,
-  saveUninitialized: true,
-  store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }),
-}))
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }),
+  })
+);
 
 app.use(express.static("public"));
-
-
-
 
 // Templating Engine
 
@@ -40,10 +39,9 @@ app.set("view engine", "ejs");
 
 app.locals.isActiveRoute = isActiveRoute;
 
-
 app.use("/", require("./server/routes/main"));
 app.use("/", require("./server/routes/admin"));
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
   console.log(`app listening on port ${PORT}`);
-})
+});
--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
-const connectDB = async ()=>{
-  try{
-    mongoose.set("strictQuery",false);
+const mongoose = require("mongoose");
+const connectDB = async () => {
+  try {
+    mongoose.set("strictQuery", false);
     const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log("Database connected");
-  }catch(error){
+  } catch (error) {
     console.log(error);
   }
-}
+};
 
 module.exports = connectDB;
